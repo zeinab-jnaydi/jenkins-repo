7f98a0e59079008c86b6fab69bcf32a62b1ad9e2
zeinajnaydi <30489870+zeinajnaydi@users.noreply.github.com>
2017-10-23 13:10:47 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {TodoForm,TodoInputs ,TodoList} from './components/todo'
+import {addTodo,generateId} from './lib/todoHelpers'
+
+class App extends Component {
+constructor(){
+  super()
+  this.state = {
+    todos: [
+    {id:1,name :'learn php',isComplete:true},
+     {id:2,name :'learn java',isComplete:false},
+      {id:3,name :'learn vb',isComplete:false}
+    ],
+   
+    currentTodo :''
+   
+  }
+this.handleInputChange = this.handleInputChange.bind(this)
+this.handleSubmit =  this.handleSubmit.bind(this)
+this.handleEmptySubmit =  this.handleEmptySubmit.bind(this)
+}
+
+
+
+    handleSubmit(evt){
+    evt.preventDefault()
+     const newId = generateId()
+     const newTodo = {id :newId, name :this.state.currentTodo,isComplete:false}
+    const updatedTodos = addTodo(this.state.todos,newTodo)
+
+  
+   this.setState ({
+     todos :updatedTodos,
+     currentTodo:'',
+     errorMessage:''
+    
+   })
+    }
+
+    handleInputChange(evt){
+    this.setState({
+    currentTodo :evt.target.value
+    })
+    }
+
+    handleEmptySubmit(evt){
+  evt.preventDefault()
+this.setState({
+  errorMessage :'please enter a name !!'
+})
+}
+
+ 
+
+  render() {
+    const submitHandler = this.state.currentTodo ? this.handleSubmit : this.handleEmptySubmit
+    
+    return (
+
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+      <div className="Todo-App">
+     
+      {this.state.errorMessage && <span className='error'>{this.state.errorMessage}</span>}
+     
+      <TodoForm handleInputChange = {this.handleInputChange}
+      currentTodo = {this.state.currentTodo}
+      handleSubmit = {submitHandler}/>
+     
+     <TodoList todos={this.state.todos}/>
+
+     
+
+       </div>
+    </div>
+    );
+  }
+}
+
+export default App;
